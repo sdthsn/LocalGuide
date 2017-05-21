92786cd618ff522d7ff6e2e73f5b0cabb93c8cb7
sdthsn <sdthsn@outlook.com>
2017-05-20 18:45:04 -0700

    Citi model addede and repository project created

---
+++ NaturalBeauty/DataModel/Common/Entity.cs
@@ -0,0 +1,25 @@
+﻿namespace Data.Common
+{
+    using System;
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+    using System.ComponentModel.DataAnnotations;
+
+    public abstract class Entity<TIdentifier>
+    {
+
+        protected Entity()
+        {
+        }
+
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        [Key]
+        [BsonElement("id")]
+        public TIdentifier Id { get; set; }
+        [BsonElement("createdOn")]
+        public DateTime CreatedOn { get; set; }
+        [BsonElement("updatedOn")]
+        public DateTime UpdatedOn { get; set; }
+    }
+}
